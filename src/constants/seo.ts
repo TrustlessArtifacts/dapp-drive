64f3aadc76c22bbc0312affb6676708661e47b16
benjiro2077 <benjiro2077@generative.xyz>
2023-04-18 17:08:04 +0700

    feat: logo

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,5 +1,5 @@
 import { CDN_URL } from "@/configs";
 
-export const SEO_TITLE = 'Trustless Domain';
-export const SEO_DESCRIPTION = 'Name your TC wallet and use your BNS name to receive any token and NFT. No more copying and pasting long addresses.';
+export const SEO_TITLE = 'Artifacts';
+export const SEO_DESCRIPTION = 'Store files on Bitcoin. Cheap. Immutable. Fully on-chain. Large files are supported.';
 export const SEO_IMAGE = `${CDN_URL}/images/display.jpg`;
