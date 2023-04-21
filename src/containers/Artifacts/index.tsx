a5168e9b413eac1764ca130ce6812f3e604c7116
archaon <archaon@generative.xyz>
2023-04-21 15:21:56 +0700

    fix: connect wallet before preserve

--- src/containers/Artifacts/index.tsx
+++ src/containers/Artifacts/index.tsx
@@ -2,26 +2,42 @@ import Text from '@/components/Text';
 import React, { useEffect, useState } from 'react';
 import { FileUploader } from 'react-drag-drop-files';
 import BFSList from './BFSList';
-import { UploadFileContainer, ArtifactWrapper, PreserveButton } from './Artifacts.styled';
+import {
+  UploadFileContainer,
+  ArtifactWrapper,
+  PreserveButton,
+} from './Artifacts.styled';
 import ModalUpload from './ModalUpload';
 import { BLOCK_CHAIN_FILE_LIMIT } from '@/constants/file';
 import toast from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+import { getIsAuthenticatedSelector } from '@/state/user/selector';
+import { useRouter } from 'next/router';
+import { ROUTE_PATH } from '@/constants/route-path';
 
 const Artifacts: React.FC = () => {
+  const router = useRouter();
   const [file, setFile] = useState<File | null>(null);
   const [showUploadModal, setShowUploadModal] = useState(false);
+  const isAuthenticated = useSelector(getIsAuthenticatedSelector);
+  console.log('🚀 ~ isAuthenticated:', isAuthenticated);
 
   const onChangeFile = (file: File): void => {
     setFile(file);
   };
 
   const onSizeError = (): void => {
-    toast.error(`File size error, maximum file size is ${BLOCK_CHAIN_FILE_LIMIT * 1000}kb.`);
+    toast.error(
+      `File size error, maximum file size is ${BLOCK_CHAIN_FILE_LIMIT * 1000}kb.`,
+    );
   };
 
   const handlePreverseArtifact = () => {
-    setShowUploadModal(true)
-  }
+    if (!isAuthenticated) router.push(ROUTE_PATH.CONNECT_WALLET);
+    else {
+      setShowUploadModal(true);
+    }
+  };
 
   useEffect(() => {
     if (file) {
@@ -35,14 +51,19 @@ const Artifacts: React.FC = () => {
         <div className="upload_left">
           <div className="upload_content">
             <h3 className="upload_title">Artifacts</h3>
-            <Text size="medium">Cheap. Immutable. Fully on-chain. Large files are supported too.</Text>
+            <Text size="medium">
+              Cheap. Immutable. Fully on-chain. Large files are supported too.
+            </Text>
           </div>
         </div>
         <div className="upload_right">
-          <PreserveButton
-            onClick={handlePreverseArtifact}
-          >
-            <Text size="medium" color="bg1" className="button-text" fontWeight="medium">
+          <PreserveButton onClick={handlePreverseArtifact}>
+            <Text
+              size="medium"
+              color="bg1"
+              className="button-text"
+              fontWeight="medium"
+            >
               Preserve Artifact
             </Text>
           </PreserveButton>
@@ -51,12 +72,17 @@ const Artifacts: React.FC = () => {
             name={'fileUploader'}
             maxSize={BLOCK_CHAIN_FILE_LIMIT}
             onSizeError={onSizeError}
-            classes={'file-uploader'}
+            classes={`file-uploader ${!isAuthenticated ? 'hidden' : ''}`}
           />
         </div>
       </UploadFileContainer>
       <BFSList />
-      <ModalUpload show={showUploadModal} handleClose={() => setShowUploadModal(false)} file={file} setFile={setFile} />
+      <ModalUpload
+        show={showUploadModal}
+        handleClose={() => setShowUploadModal(false)}
+        file={file}
+        setFile={setFile}
+      />
     </ArtifactWrapper>
   );
 };
