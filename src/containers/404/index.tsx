71562f80c51f9ecb8da6d0603702e5be69cfdaaf
benjiro2077 <benjiro2077@generative.xyz>
2023-06-07 16:28:53 +0700

    feat: 404 page

--- src/containers/404/index.tsx
+++ src/containers/404/index.tsx
@@ -1,9 +1,11 @@
+import { CDN_URL } from '@/configs';
 import { Container } from './404.styled';
 
 const Page404 = () => {
   return (
     <Container>
-      <p className="title">Page not found</p>
+      <img className='image' src={`${CDN_URL}/pages/artifacts/404-background_1.png`} alt="404 background" />
+      <h1 className="page-title">404 - Page not found</h1>
     </Container>
   );
 };
