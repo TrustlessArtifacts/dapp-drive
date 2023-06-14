d1c63a1ea610a97e98c3dd99b5b472136684d8da
Minh Tam <tam@autonomous.nyc>
2023-06-14 17:46:01 +0700

    fix: delele duplicate code, add link to owner

--- src/containers/Item/index.tsx
+++ src/containers/Item/index.tsx
@@ -96,7 +96,7 @@ const Inscription: React.FC<IProps> = ({ data }: IProps) => {
       <div className="item">
         <p className="name">{title}</p>
         {link ? (
-          <a className="link" href={link}>
+          <a className="desc" href={link} target="_blank">
             {desc}
           </a>
         ) : (
@@ -262,12 +262,7 @@ const Inscription: React.FC<IProps> = ({ data }: IProps) => {
                 renderListItem(
                   'Owner',
                   inscription?.bnsData?.[0]?.name || inscription?.owner,
-                )}
-
-              {inscription?.owner &&
-                renderListItem(
-                  'Owner',
-                  inscription?.bnsData?.[0]?.name || inscription?.owner,
+                  `https://explorer.trustless.computer/address/${inscription?.owner}`,
                 )}
 
               {inscription?.contentType &&
